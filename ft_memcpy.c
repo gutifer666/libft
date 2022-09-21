c534be6d6e86d13396ab821e2cf52d42552895c7
gutifer666 <gutifer666@gmail.com>
2022-09-21 23:02:59 +0200

    modified memcpy function

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: guti <guti@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 13:57:20 by frgutier          #+#    #+#             */
-/*   Updated: 2022/09/19 13:58:48 by frgutier         ###   ########.fr       */
+/*   Updated: 2022/09/21 23:00:04 by guti             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	while (n--)
-		*(unsigned char *)dst++ = *(unsigned char *)src++;
+	size_t	i;
+	char	*d;
+	char	*s;
+
+	i = 0;
+	d = (char *)dst;
+	s = (char *)src;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
 	return (dst);
 }
