82dd4a549755ec577763a42a3288863126ad7835
Francisco Javier Gutierrez Perez <frgutier@c2r1s3.42malaga.com>
2022-09-25 09:58:08 +0200

    null, null == null

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 13:57:20 by frgutier          #+#    #+#             */
-/*   Updated: 2022/09/23 10:04:07 by frgutier         ###   ########.fr       */
+/*   Updated: 2022/09/25 09:55:43 by frgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	i = 0;
 	d = (char *)dst;
 	s = (char *)src;
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	while (i < n)
 	{
 		d[i] = s[i];
