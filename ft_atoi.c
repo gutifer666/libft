5007c22f1b4520ddd0265329961339291992d7cc
Francisco Javier Gutierrez Perez <frgutier@c2r1s3.42malaga.com>
2022-09-25 10:57:28 +0200

    plus & space solved

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 14:15:39 by frgutier          #+#    #+#             */
-/*   Updated: 2022/09/23 08:38:17 by frgutier         ###   ########.fr       */
+/*   Updated: 2022/09/25 10:55:08 by frgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,29 @@
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	neg;
-	int	res;
+	long	i;
+	long	number;
+	int		isneg;
 
-	if (!str)
-		return (0);
 	i = 0;
-	neg = 1;
-	res = 0;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
-		|| str[i] == '\f' || str[i] == '\r')
+	number = 0;
+	isneg = 0;
+	while (str[i] != '\0' && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+			|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f'))
 		i++;
-	if (str[i] == '-')
-		neg = -1;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while (ft_isdigit(str[i]))
+	if (str[i] != '\0' && str[i] == '-')
 	{
-		res = res * 10 + str[i] - '0';
+		isneg = 1;
 		i++;
 	}
-	return (res * neg);
+	else if (str[i] == '+')
+		i++;
+	while (str[i] != '\0' && (ft_isdigit(str[i])))
+	{
+		number = (number * 10) + (str[i] - '0');
+		i++;
+	}
+	if (isneg == 1)
+		return (-number);
+	return (number);
 }
