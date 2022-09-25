a758ee2200dc50efbca48a24a9dfc7be21f58c41
Francisco Javier Gutierrez Perez <frgutier@c2r1s3.42malaga.com>
2022-09-25 09:45:41 +0200

    memmory solutions

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 13:44:05 by frgutier          #+#    #+#             */
-/*   Updated: 2022/09/19 13:46:21 by frgutier         ###   ########.fr       */
+/*   Updated: 2022/09/25 09:42:11 by frgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,15 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char	*last;
+	int		i;
 
-	last = NULL;
-	while (*s)
-	{
-		if (*s == c)
-			last = (char *)s;
-		s++;
-	}
-	if (c == '\0')
-		return ((char *)s);
-	return (last);
+	i = ft_strlen(s) - 1;
+	if (c == 0)
+		return ((char *)(s + i + 1));
+	while (i >= 0 && s[i] != c)
+		i--;
+	if (s[i] == c)
+		return ((char *)(s + i));
+	else
+		return (NULL);
 }
